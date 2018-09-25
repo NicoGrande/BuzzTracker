d1897fce15beecdc0142b0b4335fa6d893002cbc
Nico Grande <nicogrande@gatech.edu>
2018-09-25 19:03:11 -0400

    deleting useless activities

--- app/src/main/java/com/github/buzztracker/MainScreenActivity.java
+++ app/src/main/java/com/github/buzztracker/MainScreenActivity.java
@@ -1,7 +1,10 @@
 package com.github.buzztracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainScreenActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        final Button button = findViewById(R.id.button_logOut_main);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainScreenActivity.this, Login.class);
+                MainScreenActivity.this.startActivity(myIntent);
+            }
+        });
     }
+
 }
