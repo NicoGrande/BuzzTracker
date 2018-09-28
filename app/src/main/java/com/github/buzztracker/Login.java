83b11f18658432ffbbd785c8b3bfa04054375f38
Bradley Dover <dover.bradley@gmail.com>
2018-09-27 20:12:04 -0400

    Replaced password too short notification

--- app/src/main/java/com/github/buzztracker/Login.java
+++ app/src/main/java/com/github/buzztracker/Login.java
@@ -161,8 +161,8 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
+        if (!TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_empty_password));
             focusView = mPasswordView;
             cancel = true;
         }
@@ -195,12 +195,7 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
         //TODO: Replace this with your own logic
         return email.contains("@");
     }
-
-    private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
-    }
-
+    
     /**
      * Shows the progress UI and hides the login form.
      */
